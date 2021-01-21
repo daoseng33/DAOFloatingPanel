4700ce71bc6c08065233021782a0c95f0c0f4f7c
ray <daoseng33@gmail.com>
2021-01-21 18:57:28 +0800

    Initial commit

---
+++ Example/DAOFloatingPanel/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  DAOFloatingPanel
+//
+//  Created by ray on 01/21/2021.
+//  Copyright (c) 2021 ray. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
