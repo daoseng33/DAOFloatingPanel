efeb43fc9481f4666199ef7abf6f6f04b9e78da0
ray <daoseng33@gmail.com>
2021-01-22 00:32:48 +0800

    Release v 0.1.0

--- DAOFloatingPanel.podspec
+++ DAOFloatingPanel.podspec
@@ -9,7 +9,8 @@
 Pod::Spec.new do |s|
   s.name             = 'DAOFloatingPanel'
   s.version          = '0.1.0'
-  s.summary          = 'A short description of DAOFloatingPanel.'
+  s.summary          = 'A Facebook like floating panel.'
+  s.swift_versions   = '5.0'
 
 # This description is used to generate tags and improve search results.
 #   * Think: What does it do? Why did you write it? What is the focus?
@@ -21,14 +22,14 @@ Pod::Spec.new do |s|
 TODO: Add long description of the pod here.
                        DESC
 
-  s.homepage         = 'https://github.com/ray/DAOFloatingPanel'
+  s.homepage         = 'https://github.com/daoseng33/DAOFloatingPanel'
   # s.screenshots     = 'www.example.com/screenshots_1', 'www.example.com/screenshots_2'
   s.license          = { :type => 'MIT', :file => 'LICENSE' }
-  s.author           = { 'ray' => 'daoseng33@gmail.com' }
-  s.source           = { :git => 'https://github.com/ray/DAOFloatingPanel.git', :tag => s.version.to_s }
+  s.author           = { 'DAO' => 'daoseng33@gmail.com' }
+  s.source           = { :git => 'https://github.com/daoseng33/DAOFloatingPanel.git', :tag => s.version.to_s }
   # s.social_media_url = 'https://twitter.com/<TWITTER_USERNAME>'
 
-  s.ios.deployment_target = '9.0'
+  s.ios.deployment_target = '11.0'
 
   s.source_files = 'DAOFloatingPanel/Classes/**/*'
   
--- Example/DAOFloatingPanel/ViewController.swift
+++ Example/DAOFloatingPanel/ViewController.swift
@@ -7,18 +7,57 @@
 //
 
 import UIKit
+import DAOFloatingPanel
 
 class ViewController: UIViewController {
+  lazy var normalPanel: DAOFloatingPanelViewController = {
+    let panel = DAOFloatingPanelViewController(type: .normal)
+    panel.delegate = self
+    
+    return panel
+  }()
+  
+  lazy var extendablePanel: DAOFloatingPanelViewController = {
+    let panel = DAOFloatingPanelViewController(type: .extendable)
+    panel.delegate = self
+    
+    return panel
+  }()
+  
+  @IBAction func showPanel(_ sender: Any) {
+    self.present(normalPanel, animated: false, completion: nil)
+  }
+  
+  @IBAction func showExtendablePanel(_ sender: Any) {
+    self.present(extendablePanel, animated: false, completion: nil)
+  }
+  
+}
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+extension ViewController: FloatingPanelDelegate {
+  func setupFloatingPanelContentUI(panel: DAOFloatingPanelViewController) {
+    
+    let label = UILabel()
+    label.translatesAutoresizingMaskIntoConstraints = false
+    
+    switch panel {
+    case normalPanel:
+      label.text = "normal"
+    
+    case extendablePanel:
+      label.text = "extendable"
+      
+    default:
+      break
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
+    
+    panel.contentView.addSubview(label)
+    
+    NSLayoutConstraint.activate([
+      label.centerXAnchor.constraint(equalTo: panel.contentView.centerXAnchor),
+      label.centerYAnchor.constraint(equalTo: panel.contentView.centerYAnchor)
+    ])
+  }
+  
 }
 
